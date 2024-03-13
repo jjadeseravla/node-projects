439d0bb06b6bb5ad7a6f70cb81e3a4a8427fb582
Jade Alvares <jadea@webother.com>
2024-03-13 20:04:24 +0000

    moving everything to MVC so model has its own file

--- node-mvc/models/product.js
+++ node-mvc/models/product.js
@@ -1,6 +1,54 @@
+const fs = require('fs');
+const path = require('path');
 
-const products = [];
+// const p = path.join(path.dirname(require.main.filename), 'data', 'products.json');
 
+// const getProductsFromFile = (aCallback) => {
+//   fs.readFile(p, (err, fileContent) => {
+//     if (err) {
+//       return aCallback([]); // Return an empty array if file read fails
+//     }
+//     const products = JSON.parse(fileContent);
+//     aCallback(products);
+//   });
+// }
+
+// module.exports = class Product {
+//   constructor(title) {
+//     this.title = title;
+//   }
+
+//   // Store an array of products and fetch it
+//   save() {
+//     getProductsFromFile((products) => {
+//       products.push(this); // Add the current product to the array
+//       fs.writeFile(p, JSON.stringify(products), err => {
+//         console.log(err);
+//       });
+//     });
+//   }
+
+//   static fetchAll(aCallback) {
+//     getProductsFromFile(aCallback);
+//   }
+// }
+
+
+const p = path.join(path.dirname(require.main.filename),
+'data',
+'products.json'
+);
+
+const getProductsFromFile = (aCallback) => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      return aCallback([]) // dont return []
+    }
+    // aCallback(JSON.parse(fileContent));
+    const products = JSON.parse(fileContent);
+    aCallback(products);
+  });
+}
 module.exports = class Product {
   constructor(title) {
     this.title = title;
@@ -8,10 +56,15 @@ module.exports = class Product {
 
   // store an arr of products and fetch it
   save() {
-    products.push(this)
+    getProductsFromFile((products) => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), err => {
+        console.log(err);
+      });
+    });
   }
 
-  static fetchAll() {
-    return products;
+  static fetchAll(aCallback) { // aCallback refers to the anonymous fm passed into fetchAll in the controller/products.getShopProducts
+    getProductsFromFile(aCallback);
   }
 }
