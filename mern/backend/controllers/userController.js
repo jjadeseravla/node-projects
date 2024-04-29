69bc51d64b572b6f0ca42785a1e2d982fb350a8c
Jade Alvares <jadea@webother.com>
2024-04-29 16:37:21 +0100

    authenticate login users

--- mern/backend/controllers/userController.js
+++ mern/backend/controllers/userController.js
@@ -8,7 +8,15 @@ const createToken = (_id) => {
 
 //login
 const loginUser = async (req, res) => {
-  res.json({msg: 'login user'})
+  const { email, password } = req.body;
+  try {
+    const user = await User.login(email, password);
+    //create a token
+    const token = createToken(user._id);
+    res.status(200).json({email, token})
+  } catch (e) {
+    res.status(400).json({ error: e.message });
+  }
 }
 
 //signup
