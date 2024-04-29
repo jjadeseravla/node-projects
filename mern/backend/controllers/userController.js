ee62ea2f2e134a81f5e3417b6244bc0161c6826b
Jade Alvares <jadea@webother.com>
2024-04-29 16:28:30 +0100

    JWT token with headers, payload and secret all encoded and bunched together

--- mern/backend/controllers/userController.js
+++ mern/backend/controllers/userController.js
@@ -1,5 +1,10 @@
 
 const User = require('../models/userModel');
+const jwt = require('jsonwebtoken');
+
+const createToken = (_id) => {
+  return jwt.sign({ _id: _id }, process.env.SECRET, { expiresIn: '3d' });
+}
 
 //login
 const loginUser = async (req, res) => {
@@ -11,7 +16,9 @@ const signupUser = async (req, res) => {
   const { email, password } = req.body;
   try {
     const user = await User.signup(email, password);
-    res.status(200).json({email, user})
+    //creten a token
+    const token = createToken(user._id);
+    res.status(200).json({ email, token }) // the token is the headers, payload and secret all encoded and bunched together
   } catch (e) {
     res.status(400).json({ error: e.message });
   }
